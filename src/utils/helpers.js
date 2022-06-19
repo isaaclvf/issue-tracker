2f9d757dfc1b2afc11bc364bb651c9521c44913b
Isaac <isaacfarias006@gmail.com>
2022-06-19 12:46:42 -0300

    Add helper function for getting tokens

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -7,6 +7,17 @@ const formatRoute = (str) => {
     .replace(' ', '_')
 }
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7) // After 'bearer '
+  }
+
+  return null
+}
+
 module.exports = {
   formatRoute,
+  getTokenFrom,
 }
