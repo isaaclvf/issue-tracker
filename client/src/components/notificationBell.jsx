6e650901e9f75c4944a8156ffd9523d523f1014c
Isaac <isaacfarias006@gmail.com>
2022-06-24 17:48:32 -0300

    Add notification bell

---
+++ client/src/components/notificationBell.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Badge from '@mui/material/Badge';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import BasicMenu from './BasicMenu';
+
+const NotificationBell = ({ badgeContent, handleOpen, handleClose, anchorEl, open }) => {
+  const notifications = ['Foo', 'Bar', 'Baz']
+  badgeContent = notifications.length
+
+  const newNotifications = `You have ${badgeContent} new notifications`
+  const noNotifications = 'No new notifications'
+
+  return (
+    <>
+      <Tooltip
+        title={badgeContent ? newNotifications : noNotifications}
+      >
+        <IconButton
+          aria-label='notification'
+          onClick={badgeContent ? handleOpen : null}
+          anchorEl={anchorEl}
+        >
+          <Badge badgeContent={badgeContent} color="primary">
+            <NotificationsIcon color="action" />
+          </Badge>
+        </IconButton>
+      </Tooltip>
+      <BasicMenu
+        open={open}
+        anchorEl={anchorEl}
+        handleClose={handleClose}
+        menuItems={notifications}
+      />
+    </>
+  )
+}
+
+export default NotificationBell
