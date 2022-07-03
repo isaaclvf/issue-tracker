b1d7e09033eca5acd6eac98fffcf5eac02989c8f
Isaac <isaacfarias006@gmail.com>
2022-07-03 11:03:52 -0300

    Rename pages folder

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import SignUp from './routes/SignUp';
-import Login from './routes/Login';
-import Dashboard from './routes/Dashboard';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
