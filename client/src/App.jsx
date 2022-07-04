342709935d1af26ac7338892a1d9a8f805cc2028
Isaac <isaacfarias006@gmail.com>
2022-07-04 19:49:59 -0300

    Fix initial routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,16 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
 
 export default function App() {
+  const token = localStorage.getItem('token')
+  const path = token ? "/dashboard" : "/login"
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    navigate(path)
+  }, [])
+
   return (
     <Outlet />
   )
