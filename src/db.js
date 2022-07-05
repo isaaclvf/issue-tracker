2f12f83c6e9b63c7f1def1ef1c5b608744578912
Isaac <isaacfarias006@gmail.com>
2022-07-05 19:41:27 -0300

    Update db config

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg')
-const { db_info } = require('./config')
+// const { db_info } = require('./config')
 
-const { database, host, password, port, user } = db_info
+// const { database, host, password, port, user } = db_info
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
